5e08b8d8849698d67c64b5a4003aefc311b03efd
Út Phạm <utphamt>
2024-04-03 17:44:27 +0700

    commit lan 2

--- PhamThiUt.py
+++ PhamThiUt.py
@@ -1,2 +1,15 @@
 # đọc 1 file văn bản gồm nhiều dòng
 # ghi ra mà hình thứ tự ngược lại của các dòng
+import sys
+
+
+file_path = "test.txt"
+with open(file_path, "r") as file:
+    
+    lines = file.readlines()
+
+for line in reversed(lines):
+    print(line.strip())
+
+file.close()
+
